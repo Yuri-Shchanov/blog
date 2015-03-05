3b6f01a3fdf075194e5ee34b162ba92d4ce47f10
murphylol <murphy11@mail.ru>
2015-03-05 20:56:24 +0300

    test

--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
+class Article < ActiveRecord::Base
+  validates :title, presence: true
+end
+
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "the truth" do
+     assert true
+   end
+
+   test 'should not save article without title' do
+     article = Article.new
+     assert_not article.save
+   end
 end
