99ca15d4fc64fdb7704649efcdbf14e2c1bfc82e
murphylol <murphy11@mail.ru>
2015-03-10 16:58:27 +0300

    test

--- test/controllers/articles_controller_test.rb
+++ test/controllers/articles_controller_test.rb
@@ -16,4 +16,14 @@ test "should create article" do
   assert_redirected_to article_path(assigns(:article))
 end
 
+test "index should render correct template and layout" do
+  get :index
+  assert_template :index
+  assert_template layout: "layouts/application"
+end
+
+test "should route to article" do
+  assert_routing '/articles/1', {controller: "articles", action: "show", id: "1"}
+end
+
 end
