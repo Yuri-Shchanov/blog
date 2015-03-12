1c3710c52657088ced38e4cf880877840b544d4e
murphylol <murphy11@mail.ru>
2015-03-12 20:56:12 +0300

    1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,17 @@
 class UsersController < ApplicationController
   def new
+   # @user = User.new
   end
+
+  def create
+     render plain: params[:user].inspect
+#    @user = User.new(user_params)
+
+ #   if @user.save
+  #    redirect_to @user
+   # else
+    #  render 'new'
+  #  end
+  end
+
 end
