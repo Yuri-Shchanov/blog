c68be80d1e6913f1ec5cc319892623247bb48719
murphylol <murphy11@mail.ru>
2015-03-20 19:40:26 +0300

    almost working!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,7 +50,7 @@ class UsersController < ApplicationController
 
 	private
   		def user_params
-    		params.require(:user).permit(:name, :id)
+    		params.require(:user).permit(:id, :name)
   	end
 end
 
